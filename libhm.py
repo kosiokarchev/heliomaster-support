4a21723aebdd4e12781d425a1da1cc177c685af3
Kosio Karchev <kosiokarchev@gmail.com>
2018-08-23 17:10:22 +0000

    initial commit

--- libhm.py
+++ libhm.py
@@ -12,10 +12,15 @@ def _to_python(arr):
 
 
 def pointer_to_ndarray(ptr, shape, dtype):
+    ptr, shape, dtype = int(ptr), tuple(shape), str(dtype)
     cptr = c.c_void_p(ptr)
     ctype = np.ctypeslib._typecodes[np.dtype(dtype).str] # hack...
     return np.ctypeslib.as_array(c.cast(cptr, c.POINTER(ctype)), shape)
 
+def print_array(img):
+    if isinstance(img, np.ndarray):
+        with open('c:/users/kosio/desktop/pythonlog.log', 'a') as out:
+            print(img, file=out)
 
 def expcorr(img, level, func=np.mean):
     return _to_python(level / func(_normalize(img), axis=(0, 1)))
@@ -24,7 +29,7 @@ def expcorr_mean(img, level): return expcorr(img, level, np.mean)
 def expcorr_perc(img, level, perc): return expcorr(img, level, lambda x, axis: np.percentile(x, perc, axis))
 
 
-img = np.ndarray((1024, 1280, 3), dtype=np.uint16)
-img[...] = np.arange(1280*3).reshape((-1, 3))
-
-arr = pointer_to_ndarray(img.__array_interface__['data'][0], img.shape, img.dtype.name)
+# img = np.ndarray((1024, 1280, 3), dtype=np.uint16)
+# img[...] = np.arange(1280*3).reshape((-1, 3))
+#
+# arr = pointer_to_ndarray(img.__array_interface__['data'][0], img.shape, img.dtype.name)
